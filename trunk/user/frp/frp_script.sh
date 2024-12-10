4422028d4b28d024d77bf4ed15ac0a8b37c93cee
frankie540 <117900565+frankie540@users.noreply.github.com>
2024-12-10 23:40:57 +0800

    Update frp_script.sh

--- trunk/user/frp/frp_script.sh
+++ trunk/user/frp/frp_script.sh
@@ -6,20 +6,24 @@ mkdir -p /tmp/frp
 #创建frpc配置文件
 cat > "/tmp/frp/myfrpc.ini" <<-\EOF
 # ==========客户端配置：==========
+
 [common]
-server_addr = 1192.0.0.3
+server_addr = frp.104300.xyz
 server_port = 7000
-token = 12345
+token = www.126126.xyz
+
+[frankiept01]
+type = tcp
+local_ip = 192.168.100.1
+local_port = 80
+remote_port = 54023
+use_encryption = true
+use_compression = true
 
 log_file = /tmp/frpc.log
 log_level = info
 log_max_days = 3
 
-[test]
-remote_port = 6000
-type = tcp
-local_ip = 192.168.2.1
-local_port = 80
 # ================================
 EOF
 
